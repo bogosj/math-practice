76f95c5ce2aad0244a29f3b139eea67d2dbeabc6
James Bogosian <bogosj@users.noreply.github.com>
2017-06-09 16:28:58 -0400

    Create multiplication-practice.js

--- multiplication-practice.js
+++ multiplication-practice.js
@@ -1,5 +1,24 @@
+var _FIVE_MINUTES = 1 * 60 * 1000;
+var millisecondsRemaining = _FIVE_MINUTES;
+var timerId;
+
+var updateTimer = function() {
+  if (millisecondsRemaining == 0) {
+    window.clearInterval(timerId);
+    completeQuiz();
+  } else {
+    millisecondsRemaining -= 100;
+    $('#timer').text(millisecondsRemaining);
+  }
+};
+
+var completeQuiz = function() {
+  alert('Quiz over');
+};
+
 var onStartClick = function() {
-  alert('hi');
+  millisecondsRemaining = _FIVE_MINUTES;
+  timerId = window.setInterval(updateTimer, 100);
 };
 
 var onLoad = function() {
